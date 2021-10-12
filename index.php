bfecad158a759c7df99a614100253c4216107481
abahonje <abahonje78@gmail.con>
2021-10-12 10:16:08 +0700

    Menambahkan paragraf baru

--- index.php
+++ index.php
@@ -6,5 +6,6 @@
 </head>
 <body>
 	<h1>Hellow World 2</h1>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci unde nobis rerum ea possimus. Modi, repellendus voluptates quam temporibus aspernatur quisquam error dolore atque! Esse cum consequatur ipsam. Cupiditate laboriosam cumque porro laborum omnis aliquam quaerat illo sed eum nobis!</p>
 </body>
 </html>
